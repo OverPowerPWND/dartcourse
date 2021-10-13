8ccac7010f367811e3d1a6285410f98b64f1c64a
OverPowerPWND <dikoksandy@gmail.com>
2021-10-13 19:54:34 +0600

    trychange

--- itschool_lesson_1/bin/itschool_lesson_1.dart
+++ itschool_lesson_1/bin/itschool_lesson_1.dart
@@ -2,6 +2,30 @@ import 'dart:io';
 import 'package:collection/collection.dart';
 import 'package:path/path.dart';
 
+void main(List<String> args) {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // void main() {
 //   final lists = [
 //     [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89],
@@ -18,14 +42,14 @@ import 'package:path/path.dart';
 // var b = ''; строка пустая - камаз без песка
 // var c = null; камаз которого нет -
 
-void main() {
-  var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// void main() {
+//   var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  var result = [];
-  for (int i = 0; i < a.length; i++) {
-    if (a[i] % 2 == 0) {
-      result.add(a[i]);
-    }
-  }
-  print(result);
-}
+//   var result = [];
+//   for (int i = 0; i < a.length; i++) {
+//     if (a[i] % 2 == 0) {
+//       result.add(a[i]);
+//     }
+//   }
+//   print(result);
+// }
