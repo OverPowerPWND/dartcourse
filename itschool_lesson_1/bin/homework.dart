5cab583d8de3cf46ab2f920ae0612d3206df71d8
OverPowerPWND <dikoksandy@gmail.com>
2021-10-15 18:49:21 +0600

    hw_task2

--- itschool_lesson_1/bin/homework.dart
+++ itschool_lesson_1/bin/homework.dart
@@ -3,21 +3,16 @@ import 'package:collection/collection.dart';
 import 'package:path/path.dart';
 
 void main() {
-  final a = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
-  final b = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-  final result = a.intersection(b).toList();
-  print(result);
+  List a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  List b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  List c = [];
+  for (int i = 0; i < a.length; i++) {
+    if (b.contains(a[i]) && !c.contains(a[i])) {
+      c.add(a[i]);
+    }
+  }
+  print(c);
 }
-  
-
-   
-  
-// List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-//   List<int> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-//   List<int> c = [];
-//   for (int i = 0; i < a.length; i++);
-//     if 
-// }
 // List<double> first = [1,2,3,4,5,6,7];
 // List<double> second = [3,5,6,7,9,10];
 // List<double> output = [];
