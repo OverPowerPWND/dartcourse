58ca32c90cc6122d40aebbb8b9c6428fb3914de1
OverPowerPWND <dikoksandy@gmail.com>
2021-10-15 14:39:44 +0600

    HW task 2

--- itschool_lesson_1/bin/homework.dart
+++ itschool_lesson_1/bin/homework.dart
@@ -2,27 +2,42 @@ import 'dart:io';
 import 'package:collection/collection.dart';
 import 'package:path/path.dart';
 
-// void main() {
-//   final lists = [
-//     [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89],
-//     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-//   ];
-//   final commonElements =
-//       lists.fold<Set>(lists.first.toSet(), (a, b) => a.intersection(b.toSet()));
-//   print(commonElements);
-// }
-
 void main() {
-  List numlist = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+  final a = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
+  final b = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+  final result = a.intersection(b).toList();
+  print(result);
+}
+  
 
-  List evenList = [];
+   
+  
+// List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+//   List<int> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+//   List<int> c = [];
+//   for (int i = 0; i < a.length; i++);
+//     if 
+// }
+// List<double> first = [1,2,3,4,5,6,7];
+// List<double> second = [3,5,6,7,9,10];
+// List<double> output = [];
 
-  for (final i in numlist) {
-    if (i.isEven) {
-      evenList.add(i);
-    }
-  }
-  print(evenList);
-} 
+// first.forEach((element) {
+//     if(!second.contains(element)){
+//     output.add(element);
+// }
+// });
+// void main() {
+//   List numlist = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+
+//   List evenList = [];
+
+//   for (final i in numlist) {
+//     if (i.isEven) {
+//       evenList.add(i);
+//     }
+//   }
+//   print(evenList);
+// } 
 
 // второе задание
