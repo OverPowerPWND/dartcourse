e47436a89bae8d14459e265ae8202a635d5c5ee6
OverPowerPWND <dikoksandy@gmail.com>
2021-10-18 18:36:54 +0600

    Homework full tasks

--- itschool_lesson_1/bin/itschool_lesson_1.dart
+++ itschool_lesson_1/bin/itschool_lesson_1.dart
@@ -2,24 +2,90 @@ import 'dart:io';
 import 'package:collection/collection.dart';
 import 'package:path/path.dart';
 
-void main() {
-  var finger = 6;
-  if (finger == 1) {
-    print('большой палец');
-  } else if (finger == 2) {
-    print('указательный палец');
-  } else if (finger == 3) {
-    print('средний палец');
-  } else if (finger == 4) {
-    print('безымянный палец');
-  } else if (finger == 5) {
-    print('мизинец');
-  } else {
-    print('вы пришелец, больше пальцев не бывает');
-  }
-}
-// 5 переменных и поработать с ними
-// null это - "камаз"
-// var a = 'hello'; строка со значением - камаз с песком
-// var b = ''; строка пустая - камаз без песка
-// var c = null; камаз которого нет -
+// задание 1
+// void main() {
+//   var finger = 6;
+//   if (finger == 1) {
+//     print('большой палец');
+//   } else if (finger == 2) {
+//     print('указательный палец');
+//   } else if (finger == 3) {
+//     print('средний палец');
+//   } else if (finger == 4) {
+//     print('безымянный палец');
+//   } else if (finger == 5) {
+//     print('мизинец');
+//   } else {
+//     print('вы пришелец, больше пальцев не бывает');
+//   }
+// }
+
+// задание 2
+// void main() {
+//   List<int> a = [2, 3, 4, 5];
+//   int result = 1;
+//   for (int i = 0; i < a.length; i++) {
+//     result = result * a[i];
+//   }
+//   print(result);
+// }
+
+// задание 3
+// void main() {
+//   var min = 0;
+//   if (min <= 15) {
+//     print('первая четверь');
+//   } else if (min <= 30) {
+//     print('вторая четверь');
+//   } else if (min <= 45) {
+//     print('третяя четверть');
+//   } else if (min <= 59) {
+//     print('четвёртая четверть');
+//   } else {
+//     print('вы за гранью');
+//   }
+// }
+// main() {
+//   final List list = [1, 2, 3, 4, 5, 6, 7];
+//   final List newList = [];
+//   for (int i = 0; i < list.length; i++) {
+//     var string;
+//     if (i + 1 < list.length) {
+//       string = "${list[i]} and ${list[i + 1]}";
+//       i++;
+//     } else {
+//       string = "${list[i]}";
+//     }
+//     newList.add(string);
+//   }
+//   print(newList);
+// }
+// задание 4
+// void main() {
+//   var a = [12, 15, 20, 59, 79];
+//   var sum = 0;
+//   for (int i = 0; i < a.length; i++) {
+//     sum += a[i];
+//   }
+//   print(sum / 2);
+// }
+
+// задание 5
+// void main() {
+//   List a = ["a", "b", "c"];
+//   List b = [1, 2, 3];
+//   print(a + b);
+// }
+
+// задание 6
+// void main() {
+//   var lang = ["ru", "en"];
+//   var arr = "";
+//   if (arr == "ru") {
+//     print('russian language');
+//   } else if (arr == "en") {
+//     print('english language');
+//   } else {
+//     print('except error');
+//   }
+// }
