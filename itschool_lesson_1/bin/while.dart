f9c59560e4f65aec28001a5647fe18ffae883678
OverPowerPWND <dikoksandy@gmail.com>
2021-10-22 17:41:53 +0600

    switch_case

---
+++ itschool_lesson_1/bin/while.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+// задание 1
+// void main() {
+//   String city = 'Ankara';
+//   var index = 0;
+//   while (index < city.length) {
+//     print(city[index].contains('a'));
+//     index += 1;
+//   }
+// }
+
+// задание 2
+
+// void main() {
+//   var a = 0;
+//   print('введите число');
+//   final int = stdin.readLineSync();
+//   print('вы ввели: $int');
+//   while (a > 0
+// }
